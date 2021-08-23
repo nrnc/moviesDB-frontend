eed9862b18309ab6e57af339936cc49b3bba4b1d
narendra chukka <narendra.chukka@yahoo.in>
2021-08-23 22:00:01 +0530

    set up the basic frontend

--- src/App.js
+++ src/App.js
@@ -4,10 +4,13 @@ import {
   Link,
   Route,
   useParams,
+  useRouteMatch,
 } from "react-router-dom";
 import Admin from "./components/Admin";
 import Home from "./components/Home";
 import Movies from "./components/Movies";
+import { Categories } from "./components/Categories";
+import { OneMovie } from "./components/OneMovie";
 function App() {
   return (
     <Router>
@@ -27,6 +30,9 @@ function App() {
                   <Link to="/movies">Movies</Link>
                 </li>
                 <li className="list-group-item">
+                  <Link to="/by-category">Categories</Link>
+                </li>
+                <li className="list-group-item">
                   <Link to="/admin">Manage Catalogue</Link>
                 </li>
               </ul>
@@ -34,8 +40,23 @@ function App() {
           </div>
           <div className="col-md-10">
             <Switch>
-              <Route path="/movies/:id">
-                <Movie />
+              <Route path="/movies/:id" component={OneMovie} />
+              <Route exact path="/by-category">
+                <CategoryPage />
+              </Route>
+              <Route
+                exact
+                path="/by-category/drama"
+                render={(props) => <Categories {...props} title="Drama" />}
+              />
+              <Route
+                exact
+                path="/by-category/comedy"
+                render={(props) => <Categories {...props} title="Comedy" />}
+              />
+
+              <Route exact path="/by-category">
+                <CategoryPage />
               </Route>
               <Route path="/admin">
                 <Admin />
@@ -54,9 +75,22 @@ function App() {
   );
 }
 
-function Movie() {
-  let { id } = useParams();
-  return <h1>Movie {id}</h1>;
+function CategoryPage() {
+  let { path, url } = useRouteMatch();
+
+  return (
+    <div>
+      <h1>Categories</h1>
+      <ul>
+        <li>
+          <Link to={`${path}/comedy`}>Comedy</Link>
+        </li>
+        <li>
+          <Link to={`${path}/drama`}>Drama</Link>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Categories.jsx
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export class Categories extends Component {
+  render() {
+    return <h2>Category :{this.props.title}</h2>;
+  }
+}
